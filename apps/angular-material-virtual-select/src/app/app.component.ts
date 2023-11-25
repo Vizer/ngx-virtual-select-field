3f05eb29a9ffd99ad9f07c31c44a0b8d101a154b
Volodymyr Zherebnyi <volodymyr.zherebnyi@gmail.com>
2023-11-25 17:26:22 +0200

    c: setup basic form field functionality for field

--- apps/angular-material-virtual-select/src/app/app.component.ts
+++ apps/angular-material-virtual-select/src/app/app.component.ts
@@ -1,16 +1,38 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { NxWelcomeComponent } from './nx-welcome.component';
 
 import { VirtualSelectFieldComponent } from '@angular-material-virtual-select/virtual-select-field';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule, VirtualSelectFieldComponent],
+  imports: [
+    NxWelcomeComponent,
+    RouterModule,
+    VirtualSelectFieldComponent,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   selector: 'angular-material-virtual-select-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-material-virtual-select';
+  options = [
+    {
+      value: 1,
+      label: 'Option 1',
+    },
+    {
+      value: 2,
+      label: 'Option 2',
+    },
+    {
+      value: 3,
+      label: 'Option 3',
+    },
+  ];
 }
--- libs/virtual-select-field/src/lib/virtual-select-field/virtual-select-field.component.ts
+++ libs/virtual-select-field/src/lib/virtual-select-field/virtual-select-field.component.ts
@@ -1,11 +1,227 @@
-import { Component } from '@angular/core';
+import { FocusMonitor } from '@angular/cdk/a11y';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostBinding,
+  HostListener,
+  Input,
+  OnDestroy,
+  OnInit,
+  Output,
+  inject,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { MatFormFieldControl } from '@angular/material/form-field';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
+import { Observable, Subject, Subscription } from 'rxjs';
+import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
 
 @Component({
   selector: 'lib-virtual-select-field',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, OverlayModule],
   templateUrl: './virtual-select-field.component.html',
-  styleUrl: './virtual-select-field.component.css',
+  styleUrl: './virtual-select-field.component.scss',
+  providers: [
+    { provide: MatFormFieldControl, useExisting: VirtualSelectFieldComponent },
+  ],
 })
-export class VirtualSelectFieldComponent {}
+// TODO: implement single value typings
+export class VirtualSelectFieldComponent<TValue>
+  implements
+    OnInit,
+    OnDestroy,
+    MatFormFieldControl<TValue[]>,
+    ControlValueAccessor
+{
+  // eslint-disable-next-line @angular-eslint/no-input-rename
+  @Input('aria-describedby')
+  userAriaDescribedBy = '';
+
+  @Output()
+  valueChange: EventEmitter<TValue[]> = new EventEmitter<TValue[]>();
+
+  focused = false;
+  autofilled = false;
+  panelOpen = false;
+
+  readonly id = `lib-virtual-select-field-${VirtualSelectFieldComponent.nextId++}`;
+  readonly controlType = 'lib-virtual-select-field';
+
+  public ngControl: NgControl | null = inject(NgControl, { optional: true });
+
+  private _fm: FocusMonitor = inject(FocusMonitor);
+  private _elRef: ElementRef<HTMLElement> = inject(ElementRef);
+  private _stateChanges = new Subject<void>();
+
+  private _onChange: (value: TValue[]) => void = () => void 0;
+  private _onTouched: () => void = () => void 0;
+
+  private _value: TValue[] = [];
+  private _required = false;
+  private _disabled = false;
+  private _touched = false;
+  private _placeholder = '';
+  private _fmMonitorSubscription: Subscription;
+
+  constructor() {
+    if (this.ngControl != null) {
+      this.ngControl.valueAccessor = this;
+    }
+
+    this._fmMonitorSubscription = this._fm
+      .monitor(this._elRef, true)
+      .subscribe((origin) => {
+        this.focused = !!origin;
+        this._stateChanges.next();
+      });
+  }
+
+  @Input()
+  set value(value: TValue[]) {
+    if (this.value === value) {
+      return;
+    }
+
+    this._value = value ? value : [];
+
+    this._stateChanges.next();
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  @Input()
+  get placeholder(): string {
+    return this._placeholder;
+  }
+
+  set placeholder(placeholder: string) {
+    this._placeholder = placeholder;
+    this._stateChanges.next();
+  }
+
+  @Input()
+  get required(): boolean {
+    return this._required;
+  }
+
+  set required(req: boolean) {
+    this._required = coerceBooleanProperty(req);
+    this._stateChanges.next();
+  }
+
+  @Input()
+  get disabled(): boolean {
+    return this._disabled;
+  }
+
+  set disabled(value: BooleanInput) {
+    this._disabled = coerceBooleanProperty(value);
+    this._stateChanges.next();
+  }
+
+  @HostBinding('class.floating')
+  get shouldLabelFloat() {
+    return this.focused || !this.empty;
+  }
+
+  get empty() {
+    return this._value?.length === 0;
+  }
+
+  get stateChanges(): Observable<void> {
+    return this._stateChanges.asObservable();
+  }
+
+  get errorState(): boolean {
+    return !!this.ngControl?.invalid && this._touched;
+  }
+
+  @HostListener('focusin')
+  onFocusIn() {
+    if (!this.focused) {
+      this.focused = true;
+      this._stateChanges.next();
+    }
+  }
+
+  @HostListener('focusout', ['$event'])
+  onFocusOut(event: FocusEvent) {
+    if (!this._elRef.nativeElement.contains(event.relatedTarget as Element)) {
+      this._touched = true;
+      this.focused = false;
+      this._onTouched();
+      this._stateChanges.next();
+    }
+  }
+
+  ngOnInit() {
+    this._disabled = this.ngControl?.disabled ?? false;
+  }
+
+  ngOnDestroy() {
+    this._stateChanges.complete();
+    this._fm.stopMonitoring(this._elRef);
+    this._fmMonitorSubscription?.unsubscribe();
+  }
+
+  // #region ControlValueAccessor
+
+  writeValue(value: TValue[]): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: (value: TValue[]) => void) {
+    this._onChange = fn;
+  }
+
+  registerOnTouched(fn: () => void): void {
+    this._onTouched = fn;
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    this.disabled = isDisabled;
+  }
+
+  // #endregion ControlValueAccessor
+
+  setDescribedByIds(ids: string[]) {
+    const controlElement = this._elRef.nativeElement;
+
+    controlElement.setAttribute('aria-describedby', ids.join(' '));
+  }
+
+  emitValue(): void {
+    this._onChange?.(this.value);
+    this.valueChange.emit(this.value);
+  }
+
+  onContainerClick(): void {
+    this.open();
+  }
+
+  toggle(): void {
+    if (this.panelOpen) {
+      this.close();
+    } else {
+      this.open;
+    }
+  }
+
+  protected open() {
+    this.panelOpen = true;
+  }
+
+  protected close() {
+    this.panelOpen = false;
+  }
+
+  static ngAcceptInputType_required: BooleanInput;
+  static ngAcceptInputType_disabled: BooleanInput;
+
+  static nextId = 0;
+}
