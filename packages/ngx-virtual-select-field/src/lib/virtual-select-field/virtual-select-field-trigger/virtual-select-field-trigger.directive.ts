d7afb78150ea585701ebfe43dc73e5ea12088e31
Volodymyr Zherebnyi <volodymyr.zherebnyi@gmail.com>
2024-05-18 22:58:10 +0300

    b: disable angular-eslint/directive-selector rule

--- packages/ngx-virtual-select-field/src/lib/virtual-select-field/virtual-select-field-trigger/virtual-select-field-trigger.directive.ts
+++ packages/ngx-virtual-select-field/src/lib/virtual-select-field/virtual-select-field-trigger/virtual-select-field-trigger.directive.ts
@@ -6,7 +6,6 @@ export const NGX_VIRTUAL_SELECT_FIELD_TRIGGER =
   );
 
 @Directive({
-  // eslint-disable-next-line @angular-eslint/directive-selector
   selector: 'ngx-virtual-select-field-trigger',
   providers: [
     {
