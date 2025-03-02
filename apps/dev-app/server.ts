90da53b21c8a6a7e4efbeb78bf59911c0ea1ad48
Volodymyr Zherebnyi <volodymyr.zherebnyi@gmail.com>
2025-03-02 15:28:22 +0200

    chore(dev-app): fix import

--- apps/dev-app/server.ts
+++ apps/dev-app/server.ts
@@ -1,5 +1,5 @@
 import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
+import { CommonEngine } from '@angular/ssr/node';
 import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
@@ -39,8 +39,8 @@ export function app(): express.Express {
         publicPath: browserDistFolder,
         providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
       })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
+      .then((html: string) => res.send(html))
+      .catch((err: Error) => next(err));
   });
 
   return server;
